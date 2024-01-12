775bded40fec4903ea4962de3e4445fdd607a906
skidoodle <contact@albert.lol>
2024-01-12 22:51:17 +0100

    "API Routes Response Size Limited to 4MB"

--- src/pages/api/proxy.ts
+++ src/pages/api/proxy.ts
@@ -1,5 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
