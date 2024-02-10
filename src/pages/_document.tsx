0302409a5ffc3f2233813eee930f9c97fd254a89
skidoodle <contact@albert.lol>
2024-02-10 19:37:35 +0100

    Add Open Graph meta tags and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,19 @@ export default function Document() {
       <Head>
         <meta name='theme-color' content='#121212' />
         <meta name='title' content='Érettségi kereső' />
+        <meta name='og:title' content='Érettségi kereső' />
+        <meta property='og:url' content='https://erettsegi.albert.lol' />
+        <meta
+          name='description'
+          content='Egyszerű keresés és letöltés az érettségi feladatsorokhoz. 🏫'
+        />
+        <meta
+          name='og:description'
+          content='Egyszerű keresés és letöltés az érettségi feladatsorokhoz. 🏫'
+        />
+        <link rel='icon' href='/favicon.ico' />
+        <meta property='image' content='/logo.png' />
+        <meta property='og:image' content='/logo.png' />
         <meta name='author' content='albert' />
       </Head>
       <body>
